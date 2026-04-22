81d26fed12a1df886f4e3e37e21d070dd24b6782
Tz | Hzk Zzz <yansaigre@gmail.com>
2026-04-22 00:26:01 -0400

    Update loader.lua

--- WindUI-main/loader.lua
+++ WindUI-main/loader.lua
@@ -1,1 +1,1 @@
-loadstring(game:HttpGet("https://raw.githubusercontent.com/TzHzk/WindUI-Modificado/main/WindUI-main/hub.lua"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Txzp/Astras-Zzz/main/WindUI-main/hub.lua"))()
